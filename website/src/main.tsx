8cf78f42fb1d472ef85dd49a03df3a5fa47a2f80
Loïc GUEZO <loicguezo@gmail.com>
2025-08-01 21:58:43 +0200

    test: config and webpage

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+
+import "./index.css"
+
+// https://tailwindcss.com/plus/templates#browse
+
+const Header: React.FC = () => {
+  return (
+    <div></div>
+  );
+};
+
+
+const Footer: React.FC = () => {
+  return (
+    <div></div>
+  );
+};
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hello there! Loïc here.</h1>
+    <div className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-gray-200 font-mono">
+      <Header/>
+      <Footer/>
     </div>
   );
 };
---
+++ website/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'media',
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                custom: '#040711',
+            },
+        },
+    },
+    plugins: [],
+};

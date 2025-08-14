74e09e9113c325bbb4237bfee6009552807f459c
Loïc GUEZO <loicguezo@gmail.com>
2025-08-14 14:04:13 +0200

    feat: rework html headers

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,31 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import "./index.css"
-
-// https://tailwindcss.com/plus/templates#browse
-
-const Header: React.FC = () => {
-  return (
-    <div></div>
-  );
-};
-
-
-const Footer: React.FC = () => {
-  return (
-    <div></div>
-  );
-};
-
-const App: React.FC = () => {
-  return (
-    <div className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-gray-200 font-mono">
-      <Header/>
-      <Footer/>
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+  </React.StrictMode>
+);

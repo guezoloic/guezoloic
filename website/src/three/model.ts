8351a623f77ee106902533fd93dd740ebd740c1a
Loïc GUEZO <loicguezo@gmail.com>
2025-08-19 18:22:26 +0200

    feat(model): move function to init()

--- website/src/three/model.ts
+++ website/src/three/model.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 export default class Model {
     private model: THREE.Object3D | null = null;
@@ -9,24 +9,21 @@ export default class Model {
     constructor(loader: GLTFLoader) {
         this.loader = loader;
     }
-    
+
     public async init(model_url: string, scene: THREE.Scene) {
-        await this.loadModel(model_url, scene);
+        this.model = await this.loadModel(model_url);
+        scene.add(this.model);
+        this.mixer = new THREE.AnimationMixer(this.model);
     }
-    
-    private loadModel(model_url: string, scene: THREE.Scene): Promise<THREE.Object3D> {
+
+    private loadModel(model_url: string): Promise<THREE.Object3D> {
         return new Promise((resolve, reject) => {
             this.loader.load(
                 model_url,
-                (gltf) => {
-                    this.model = gltf.scene;
-                    scene.add(this.model);
-                    this.mixer = new THREE.AnimationMixer(this.model);
-                    resolve(this.model);
-                },
+                (gltf: GLTF) => resolve(gltf.scene),
                 undefined,
                 reject
-            )
+            );
         });
     }
 
@@ -34,6 +31,10 @@ export default class Model {
         this.mixer?.update(delta);
     }
 
-    public getMixer(): THREE.AnimationMixer { return this.mixer }
+    public getMixer(): THREE.AnimationMixer { 
+        if (!this.mixer) throw new Error("Mixer not initialized yet!"); 
+        return this.mixer 
+    }
+    
     public getModel(): THREE.Object3D | null { return this.model; }
 }
